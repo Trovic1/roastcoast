521de3fd9272cdcfe4e452cdee75294062ad5098
Aniekan Victory <trovic101@gmail.com>
2026-04-20 14:56:53 +0100

    last update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-}
+  /* config options here */
+  // Make sure 'output: "standalone"' is completely removed
+  reactStrictMode: true, 
+};
 
-export default nextConfig
+export default nextConfig;
